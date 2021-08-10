e9b7cdc9427ddb255a078b6e6b659f1e36de1c5e
liubom <liubom@gmail.com>
2021-08-10 16:28:52 +0300

    ...

--- src/app/users/user-register/user-register.component.ts
+++ src/app/users/user-register/user-register.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {UserRegisterService} from "../user-register.service";
 import {map} from "rxjs/operators";
-import { NgForm} from "@angular/forms";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-user-register',
@@ -20,19 +20,20 @@ export class UserRegisterComponent implements OnInit {
 
   register(userForm: NgForm): void {
     if (userForm.invalid) {
-      console.log('Invalid Input');
       return;
     }
-
-    console.log(userForm.form.value.username);
 
     let {username, password} = userForm.form.value;
     this.userRegisterService
       .getUsers().pipe(
-      map((u) => u.filter((x) => {return x.username === username})))
-      .subscribe(data => {console.log(data.length); return data.length > 0
-        ? console.log('This user exists!')
-        : this.userRegisterService.addUser(username, password, this.isAdmin).subscribe(data => this.newUser = data);})
+      map((u) => u.filter((x) => {
+        return x.username === username;
+      })))
+      .subscribe(data => {
+        return data.length > 0
+          ? console.log('This user exists!')
+          : this.userRegisterService.addUser(username, password, this.isAdmin).subscribe(data => this.newUser = data);
+      })
 
     userForm.reset('');
   }
